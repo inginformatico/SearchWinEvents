888af43b950fbbc9e8a2b9fcd5cd7db8a98662ba
inginformatico <43171423+inginformatico@users.noreply.github.com>
2023-08-16 13:34:43 +0200

    Update SearchWinEvents.ps1: Escribe resultados a fichero
    Se añade variable con el nombre del fichero en el que se escriben los resultados

--- SearchWinEvents.ps1
+++ SearchWinEvents.ps1
@@ -2,7 +2,8 @@
 $computerName = "RemoteComputerName" #Escribe aquí el nombre de la máquina objetivo
 $eventLogName = "Application" #Escribe aquí el nombre del log de eventos system, application, setup, security
 $eventID = "1000","1001","1002" #Escribe aquí la lista de ids de eventos que te interesan separados por coma
+$FileOutput = "C:\eventos.txt" #Ruta y nombre del fichero en el que se escribirá la salida
 $startTime = (Get-Date).AddDays(-7)
 $endTime = Get-Date
 
-Get-WinEvent -ComputerName $computerName -FilterHashtable @{Logname=$eventLogName;ID=$eventID;StartTime=$startTime;EndTime=$endTime} | Select-Object TimeCreated,ID,Message | Format-Table -AutoSize
+Get-WinEvent -ComputerName $computerName -FilterHashtable @{Logname=$eventLogName;ID=$eventID;StartTime=$startTime;EndTime=$endTime} | Select-Object TimeCreated,ID,Message | Format-Table -AutoSize  | Out-File -FilePath $FileOutput
