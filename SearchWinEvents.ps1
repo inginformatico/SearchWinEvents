776f3d537e5817d5c235cf73ed9f231156a78666
inginformatico <43171423+inginformatico@users.noreply.github.com>
2023-08-16 11:55:04 +0200

    Create SearchWinEvents.ps1
    Código del script

---
+++ SearchWinEvents.ps1
@@ -0,0 +1,8 @@
+# Script powershell para obtener la lista de últimos eventos de un LogEvent y EventId en Windows
+$computerName = "RemoteComputerName" #Escribe aquí el nombre de la máquina objetivo
+$eventLogName = "Application" #Escribe aquí el nombre del log de eventos system, application, setup, security
+$eventID = "1000","1001","1002" #Escribe aquí la lista de ids de eventos que te interesan separados por coma
+$startTime = (Get-Date).AddDays(-7)
+$endTime = Get-Date
+
+Get-WinEvent -ComputerName $computerName -FilterHashtable @{Logname=$eventLogName;ID=$eventID;StartTime=$startTime;EndTime=$endTime} | Select-Object TimeCreated,ID,Message | Format-Table -AutoSize
