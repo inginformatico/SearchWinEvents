f9007daafb2ed30a9fa0a23a77af7df97800815a
inginformatico <43171423+inginformatico@users.noreply.github.com>
2023-08-16 13:44:05 +0200

    Update SearchWinEvents.ps1: escribe resultado a fichero

--- SearchWinEvents.ps1
+++ SearchWinEvents.ps1
@@ -2,8 +2,15 @@
 $computerName = "RemoteComputerName" #Escribe aquí el nombre de la máquina objetivo
 $eventLogName = "Application" #Escribe aquí el nombre del log de eventos system, application, setup, security
 $eventID = "1000","1001","1002" #Escribe aquí la lista de ids de eventos que te interesan separados por coma
-$FileOutput = "C:\eventos.txt" #Ruta y nombre del fichero en el que se escribirá la salida
+$FileOutput = "C:\eventos.txt" #Ruta y nombre del fichero en el que se escribirá la salida, si lo dejas vacío escribe en consola
 $startTime = (Get-Date).AddDays(-7)
 $endTime = Get-Date
 
-Get-WinEvent -ComputerName $computerName -FilterHashtable @{Logname=$eventLogName;ID=$eventID;StartTime=$startTime;EndTime=$endTime} | Select-Object TimeCreated,ID,Message | Format-Table -AutoSize  | Out-File -FilePath $FileOutput
+if ("" -eq $FileOutput)
+{
+  Get-WinEvent -ComputerName $computerName -FilterHashtable @{Logname=$eventLogName;ID=$eventID;StartTime=$startTime;EndTime=$endTime} | Select-Object TimeCreated,ID,Message | Format-Table -AutoSize
+}
+else
+{
+    Get-WinEvent -ComputerName $computerName -FilterHashtable @{Logname=$eventLogName;ID=$eventID;StartTime=$startTime;EndTime=$endTime} | Select-Object TimeCreated,ID,Message | Format-Table -AutoSize | Out-File -FilePath $FileOutput
+}
